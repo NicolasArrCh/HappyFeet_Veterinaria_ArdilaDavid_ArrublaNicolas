b335538ea03a728e28f07657279a0125b02f2a17
davida282 <davidardila1228@gmail.com>
2025-09-27 23:17:26 -0500

    Agregacion de archivosDAO

---
+++ scr/com/happyfeet/repository/DAO/ProductoTipoDAO.java
@@ -0,0 +1,101 @@
+package com.happyfeet.repository.DAO;
+
+import com.happyfeet.controller.ConexionBD;
+import com.happyfeet.model.entities.ProductoTipo;
+import com.happyfeet.model.entities.ProductoTipo.*;
+import com.happyfeet.repository.inter.IProductoTipoDAO;
+
+import java.security.ProtectionDomain;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoTipoDAO implements IProductoTipoDAO {
+
+    private Connection con;
+
+    public ProductoTipoDAO() {
+        con = ConexionBD.getConnection();
+    }
+
+    @Override
+    public void agregarProductoTipo(ProductoTipo productoTipo) {
+        String sql = "Insert into producto_tipos (nombre) values (?)";
+
+        try(PreparedStatement pstmt = con.prepareStatement(sql)){
+            pstmt.setString(2, productoTipo.getNombre());
+            pstmt.executeUpdate();
+        }catch (SQLException e) {
+            throw new RuntimeException("Error al agregar los productos" + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<ProductoTipo> obtenerTodosProductoTipos() {
+        List<ProductoTipo> lst = new ArrayList<>();
+        String sql = "select * from producto_tipos";
+
+        try(Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                ProductoTipo pta = new ProductoTipo(
+                        rs.getInt("Id"),
+                        rs.getString("nombre"));
+
+                lst.add(pta);
+            }
+        }catch (SQLException e){
+            System.out.println("Error al consultar todos los productos");
+        }
+
+        return lst;
+    }
+
+    @Override
+    public ProductoTipo obtenerProductoTipoPorId(Integer id) {
+        ProductoTipo pta = null;
+
+        String sql = "select * from producto_tipos where id = ?";
+
+        try(PreparedStatement stmt = con.prepareStatement(sql);){
+
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    pta = new ProductoTipo(
+                            rs.getInt("id"),
+                            rs.getString("nombre"));
+                }
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("Error al consultar todas los productos" + id);
+        }
+
+        return pta;
+    }
+
+    @Override
+    public void actualizarProductoTipo(ProductoTipo productoTipo) {
+        String sql = "update producto_tipo set nombre = ?, where id = ?";
+
+        try(PreparedStatement pstmt = con.prepareStatement(sql)){
+            pstmt.setString(1, productoTipo.getNombre());
+            pstmt.executeUpdate();
+        }catch (SQLException e) {
+            throw new RuntimeException("Error al actualizar el producto " + productoTipo);
+        }
+    }
+
+    @Override
+    public void eliminarProductoTipo(Integer id) {
+        String sql = "delete from producto_tipos where id = ?";
+
+        try(PreparedStatement pstmt = con.prepareStatement(sql)){
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        }catch (SQLException e) {
+            throw new RuntimeException("Error al eliminar el producto ID: " + id);
+        }
+    }
+}
---
+++ scr/com/happyfeet/repository/DAO/VeterinarioDAO.java
@@ -0,0 +1,117 @@
+package com.happyfeet.repository.DAO;
+
+import com.happyfeet.controller.ConexionBD;
+import com.happyfeet.model.entities.Veterinario;
+import com.happyfeet.model.entities.Veterinario.*;
+import com.happyfeet.repository.inter.IVeterinarioDAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VeterinarioDAO implements IVeterinarioDAO {
+
+    private Connection con;
+
+    public VeterinarioDAO() {
+        con = ConexionBD.getConnection();
+    }
+
+    @Override
+    public void agregarVeterinario(Veterinario veterinario) {
+        String sql = "Insert into veterinarios (nombre_completo, documento_identidad, especialidad, telefono, email) values (?, ?, ?, ?, ?)";
+
+        try(PreparedStatement pstmt = con.prepareStatement(sql)){
+            pstmt.setString(1, veterinario.getNombreCompleto());
+            pstmt.setString(2, veterinario.getDocumentoIdentidad());
+            pstmt.setString(3, veterinario.getEspecialidad());
+            pstmt.setString(4, veterinario.getTelefono());
+            pstmt.setString(5, veterinario.getEmail());
+
+            pstmt.executeUpdate();
+        }catch (SQLException e) {
+            throw new RuntimeException("Error al agregar los veterinarios" + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Veterinario> obtenerTodasVeterinario() {
+        List<Veterinario> lst = new ArrayList<>();
+        String sql = "select * from veterinarios";
+
+        try(Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Veterinario vt = new Veterinario(
+                        rs.getInt("Id"),
+                        rs.getString("nombre_completo"),
+                        rs.getString("documento_identidad"),
+                        rs.getString("especialidad"),
+                        rs.getString("telefono"),
+                        rs.getString("email"));
+
+                lst.add(vt);
+            }
+        }catch (SQLException e){
+            System.out.println("Error al consultar todos los veterinarios");
+        }
+
+        return lst;
+    }
+
+    @Override
+    public Veterinario obtenerVeterinarioPorId(Integer id) {
+        Veterinario vt = null;
+
+        String sql = "select * from veterinarios where id = ?";
+
+        try(PreparedStatement stmt = con.prepareStatement(sql);){
+
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    vt = new Veterinario(
+                            rs.getInt("id"),
+                            rs.getString("nombre_completo"),
+                            rs.getString("documento_identidad"),
+                            rs.getString("especialidad"),
+                            rs.getString("telefono"),
+                            rs.getString("email"));
+                }
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("Error al consultar todos los veterinarios" + id);
+        }
+
+        return vt;
+    }
+
+    @Override
+    public void actualizarVeterinario(Veterinario veterinario) {
+        String sql = "update veterinarios set nombre_completo = ?, documento_identidad = ?, especialidad = ?, telefono = ?, email = ?, where id = ?";
+
+        try(PreparedStatement pstmt = con.prepareStatement(sql)){
+            pstmt.setString(1, veterinario.getNombreCompleto());
+            pstmt.setString(2, veterinario.getDocumentoIdentidad());
+            pstmt.setString(3, veterinario.getEspecialidad());
+            pstmt.setString(4, veterinario.getTelefono());
+            pstmt.setString(5, veterinario.getEmail());
+            pstmt.executeUpdate();
+        }catch (SQLException e) {
+            throw new RuntimeException("Error al actualizar el veterinario " + veterinario);
+        }
+    }
+
+    @Override
+    public void eliminarVeterinario(Integer id) {
+        String sql = "delete from veterinarios where id = ?";
+
+        try(PreparedStatement pstmt = con.prepareStatement(sql)){
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        }catch (SQLException e) {
+            throw new RuntimeException("Error al eliminar el veterinario ID: " + id);
+        }
+    }
+}
